ed1a2431fa6d76f2b39bb8a71523722248da021e
Leonardo Anjos <leodrums11@gmail.com>
2023-10-22 00:14:12 -0300

    feat: global state management
    install redux-toolkit and create store and connect it

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Router from 'routes/Router';
+import store from 'store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <Provider store={store}>
+      <RouterProvider router={Router} />
+    </Provider>
   </React.StrictMode>,
 );
