cf0b333dc78d3525e35b34ea7014e5be54a84b9a
Leonardo Anjos <leodrums11@gmail.com>
2023-10-22 13:20:12 -0300

    feat: thunk functions
    wrapping thunk functions with hooks

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -4,7 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store';
 import { Theme, setTheme } from 'store/slices/appSlice';
 
-const useTheme = () => {
+interface UseTheme {
+  theme: Theme;
+  setCurrentTheme: (theme: Theme) => void;
+  toggleTheme: () => void;
+}
+
+const useTheme = (): UseTheme => {
   const dispatch = useDispatch();
 
   const { theme } = useSelector((state: RootState) => state.app);
@@ -13,7 +19,15 @@ const useTheme = () => {
     dispatch(setTheme(theme));
   }, []);
 
-  return { theme, setCurrentTheme };
+  const toggleTheme = useCallback(() => {
+    if (theme === Theme.Light) {
+      setCurrentTheme(Theme.Dark);
+    } else if (theme === Theme.Dark) {
+      setCurrentTheme(Theme.Light);
+    }
+  }, [theme, setCurrentTheme]);
+
+  return { theme, setCurrentTheme, toggleTheme };
 };
 
 export default useTheme;
